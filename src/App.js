34969febfc5f4dfe5e917fba207889bf50fa203a
unknown <shyusupova4@gmail.com>
2022-09-19 20:06:46 +0500

    Add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path="/" />
+          <Route path="/categories" />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Book.js
@@ -0,0 +1,20 @@
+function Books() {
+  // object containing books
+  const book = [
+    {
+      id: 1,
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+    },
+    {
+      id: 2,
+      title: "Harry Potter and the Sorcerer's Stone",
+      author: 'J.K. Rowling',
+    },
+    {
+      id: 3,
+      title: 'To Kill a Mockingbird',
+      author: 'Harper Lee',
+    },
+  ];
+}
